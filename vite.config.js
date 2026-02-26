7580a4fa394a4fd5fcb1adc7e5e459f7b20ba8f8
gustavo gonçalves <gustavogs839>
2026-02-26 20:36:04 -0300

    Limpeza total e preparo para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/calculo-motorista/", // O NOME EXATO DO SEU REPOSITÓRIO NO GITHUB
+  base: "/calculo-motorista/", 
 })

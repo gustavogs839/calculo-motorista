6cfe7ba3b29c716e636db0806d233e19161734fe
gustavo gonçalves <gustavogs839>
2026-02-26 21:06:53 -0300

    usando docs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/calculo-motorista/", 
+  base: "/calculo-motorista/",
+  build: {
+    outDir: 'docs' // Vamos mudar de 'dist' para 'docs'
+  }
 })
